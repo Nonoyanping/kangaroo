f3d7f6d54529dc7ce05d427934d3f2a1e3828a7e
yanping <snowman01956172@gmail.com>
2024-01-29 17:23:06 +0900

    UI improvement

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,25 @@
 extern crate rocket;
 
 use rand::seq::SliceRandom;
-use rocket::State;
+use rocket::{response::content, State};
 
 struct AppState {
     dictionary: Vec<&'static str>,
 }
 
 #[get("/get_sentence")]
-fn get_sentence(State: State<AppState>) -> String {}
+fn get_sentence(state: State<AppState>) -> String {
+    let sentence = state
+        .dictionary
+        .choose(&mut rand::thread_rng())
+        .unwrap_or(&"Default sentence");
+    sentence.to_string()
+}
+
+#[get("/custom_test/<content>")]
+fn custom_test(content: String) -> String {
+    content
+}
 
 fn main() {
     println!("Hello, world!");
